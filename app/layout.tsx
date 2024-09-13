aff3b4e876088359310702fcf37f21ad58f399b5
casperreiff <ckr@novicell.dk>
2024-09-13 13:18:58 +0200

    Made the entire header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,10 @@ import { Inter } from 'next/font/google';
 import { ReactNode, Suspense } from 'react';
 import './globals.css';
 
-const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
+const { SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
-const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
-const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
@@ -21,14 +19,9 @@ export const metadata = {
     follow: true,
     index: true
   },
-  ...(twitterCreator &&
-    twitterSite && {
-      twitter: {
-        card: 'summary_large_image',
-        creator: twitterCreator,
-        site: twitterSite
-      }
-    })
+  icons: {
+    icon: '/icon.png'
+  }
 };
 
 const inter = Inter({
